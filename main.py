b82f0df808ad7f2888449e92f2959029442e84e3
Samir-Zade <samirzadeofficial@gmail.com>
2024-08-03 13:19:20 +0530

    training added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+
 
 # Define the name of the stage we are currently working on. This will help in logging and debugging purposes.
 STAGE_NAME = "Data Ingestion stage"
@@ -25,3 +27,16 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+#training the data
+STAGE_NAME = "Training"
+try: 
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -3,7 +3,8 @@ from src.cnnClassifier.constants import *
 from src.cnnClassifier.utils.common import read_yaml, create_directories
 from src.cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                     PrepareBaseModelConfig,
-                                                    PrepareCallbacksConfig)
+                                                    PrepareCallbacksConfig,
+                                                    TrainingConfig)
 
 #5. create and update configmanager for config->configuration.py
 class ConfigurationManager:
@@ -69,3 +70,27 @@ class ConfigurationManager:
         )
 
         return prepare_callback_config
+
+
+    #crete the model trainer
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chicken-fecal-images")
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
